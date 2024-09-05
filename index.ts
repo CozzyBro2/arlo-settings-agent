23862c90a73492df6f0a5c013641340c4baac14e
CozzyBro2 <gojinhan2@gmail.com>
2024-09-05 01:02:47 -0400

    Expand API and tweak more settings

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 
+// TODO: Read serial numbers from config
 const devices = ["A0B1917XA2F1D", "A0B1917GA2B29"]
 
 const api = axios.create ({
@@ -17,21 +18,45 @@ const quality_config = {
     quality: "medium"
 }
 
+const audio_config = {
+    enabled: false
+}
+
+const pir_config = {
+    enabled: false,
+    sensitivity: 80
+}
+
+function updateDevice(url: string, device: string, config: {}) {
+    const endpoint = `/device/${device}/${url}`
+
+    api.post(endpoint, config)
+        .then(function(res) {
+            if (res.data.result === false) {
+                throw new Error("Unknown failure (result is false)")
+            }
+            console.log(`POST ${endpoint} succesful.`)
+        }).catch(function(err) {
+            console.log(`POST ${endpoint} failed: \n${err.message}`)
+    })
+}
+
 function updateState(url: string, config: {}) {
-    for (const name of devices) {
-        api.post(`/device/${name}/${url}`, config)
-            .then(function() {
-                console.log(`POST /device/${name}/${url} succesful.`)
-            }).catch(function(err) {
-                console.log(`POST /device/${name}/${url} failed: \n${err.message}`)
-            })
+    for (const device of devices) {
+        updateDevice(url, device, config)   
     }
 }
 
+// TODO: Parse a user defined mapping of things to configure.
 function handleAPI() {
-    updateState("arm", unarm_config)
+    updateDevice("friendlyname", devices[0], {name: "Front"})
+    updateDevice("friendlyname", devices[1], {name: "Back"})
+
+    updateState("audiospeaker", audio_config)
+    updateState("audiomic", audio_config)
+    updateState("pirled", pir_config)
     updateState("quality", quality_config)
 }
 
 handleAPI()
-setInterval(handleAPI, 30000)
+setInterval(handleAPI, 200000)
